47ad8b8215cc8de1146f7385f1a6dd95a0992182
nev3rm0re <german.rumm@gmail.com>
2025-01-08 13:58:55 +0000

    v0.2.4

--- packages/server/scripts/publish.js
+++ packages/server/scripts/publish.js
@@ -49,6 +49,7 @@ async function publish() {
     // Bump version
     console.log('📝 Bumping version...');
     if (response.bump === 'other') {
+      console.log('Enter the new version:');
       execSync('yarn version');
     } else {
       execSync(`yarn version --new-version ${response.bump}`);
